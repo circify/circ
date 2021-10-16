b674a5769fab572cb9f8031e7cd7494561a49024
Alex Ozdemir <aozdemir@hmc.edu>
2021-10-15 20:28:16 -0700

    Use grep, not rg.

--- scripts/test_datalog.zsh
+++ scripts/test_datalog.zsh
@@ -13,9 +13,9 @@ $BIN --lang datalog ./examples/datalog/inv.pl || true
 $BIN --lang datalog ./examples/datalog/call.pl || true
 $BIN --lang datalog ./examples/datalog/arr.pl || true
 # Small R1cs b/c too little recursion.
-size=$(($BIN --lang datalog ./examples/datalog/dumb_hash.pl -r 4 || true) | rg "Final R1cs size:" | rg -o "\\b\\d+")
+size=$(($BIN --lang datalog ./examples/datalog/dumb_hash.pl -r 4 || true) | egrep "Final R1cs size:" | egrep -o "\\b[0-9]+")
 [ "$size" -lt 10 ]
 # Big R1cs b/c enough recursion
-($BIN --lang datalog ./examples/datalog/dumb_hash.pl -r 5 || true) | rg "Final R1cs size: 306"
-($BIN --lang datalog ./examples/datalog/dumb_hash.pl -r 10 || true) | rg "Final R1cs size: 306"
-($BIN --lang datalog ./examples/datalog/dec.pl -r 2 || true) | rg "Final R1cs size: 306"
+($BIN --lang datalog ./examples/datalog/dumb_hash.pl -r 5 || true) | egrep "Final R1cs size: 306"
+($BIN --lang datalog ./examples/datalog/dumb_hash.pl -r 10 || true) | egrep "Final R1cs size: 306"
+($BIN --lang datalog ./examples/datalog/dec.pl -r 2 || true) | egrep "Final R1cs size: 306"
