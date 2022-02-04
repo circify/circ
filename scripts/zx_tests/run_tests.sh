e45abb30eca35644076e077e033f64a891394d14
kwantam <kwantam@gmail.com>
2022-02-04 15:20:54 -0500

    small fixes ; move zx_tests

--- scripts/zx_tests/run_tests.sh
+++ scripts/zx_tests/run_tests.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 TESTDIR=$(dirname -- "$0")
-ZXI=${TESTDIR}/../target/release/examples/zxi
+ZXI=${TESTDIR}/../../target/release/examples/zxi
 
+echo Running zx should-pass tests:
 for i in ${TESTDIR}/*.zx; do
     ${ZXI} "$i" &>/dev/null
     if [ "$?" != "0" ]; then
         echo "[failure: should-pass] $i"
     fi
 done
+echo Done.
+echo
 
+echo Running zx should-fail tests:
 for i in ${TESTDIR}/*.zxf; do
     ${ZXI} "$i" &>/dev/null
     if [ "$?" == "0" ]; then
         echo "[failure: should-fail] $i"
     fi
 done
+echo Done.
