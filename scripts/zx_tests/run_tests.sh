d5fea7db8bdb7cc67160809d56b9c79d1a073b97
Alex Ozdemir <aozdemir@hmc.edu>
2022-02-05 22:15:25 -0800

    Addressing my unsolicited comment

--- scripts/zx_tests/run_tests.sh
+++ scripts/zx_tests/run_tests.sh
@@ -2,12 +2,14 @@
 
 TESTDIR=$(dirname -- "$0")
 ZXI=${TESTDIR}/../../target/release/examples/zxi
+error=0
 
 echo Running zx should-pass tests:
 for i in ${TESTDIR}/*.zx; do
     ${ZXI} "$i" &>/dev/null
     if [ "$?" != "0" ]; then
         echo "[failure: should-pass] $i"
+        error=1
     fi
 done
 echo Done.
@@ -18,6 +20,9 @@ for i in ${TESTDIR}/*.zxf; do
     ${ZXI} "$i" &>/dev/null
     if [ "$?" == "0" ]; then
         echo "[failure: should-fail] $i"
+        error=1
     fi
 done
 echo Done.
+
+exit $error
