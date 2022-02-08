362efecf608fbf247ecb1e3ca77c6eba760283a7
Edward Chen <edwjchen@gmail.com>
2022-02-08 17:01:18 -0500

    forgot debug stmt

--- src/target/aby/trans.rs
+++ src/target/aby/trans.rs
@@ -8,7 +8,7 @@ use crate::ir::term::*;
 use crate::target::aby::assignment::ilp::assign;
 use crate::target::aby::assignment::{ShareType, SharingMap};
 use crate::target::aby::utils::*;
-// use log::debug;
+use log::debug;
 use std::fmt;
 
 use std::path::Path;
@@ -615,16 +615,13 @@ pub fn to_aby(ir: Computation, path: &Path, lang: &str, cm: &str) {
         metadata: md,
         values: _,
     } = ir.clone();
-    for t in terms.clone() {
-        println!("terms: {}", t);
-    }
 
     let s_map: SharingMap = assign(&ir, cm);
     // let s_map: SharingMap = some_arith_sharing(&ir);
     let mut converter = ToABY::new(md, s_map, path, lang);
 
     for t in terms {
-        println!("terms: {}", t);
+        debug!("terms: {}", t);
         converter.lower(t.clone());
     }
 
