4e497887233946ffa13d6a1e0ebe30c128e5006b
Edward Chen <edwjchen@gmail.com>
2022-04-27 21:17:16 -0400

    broken playground

--- examples/C/mpc/playground.c
+++ examples/C/mpc/playground.c
@@ -1,13 +1,3 @@
-int t() {
-    int i;
-    if (0 == 0) {
-        i = 10;
-    } else {
-        i = 20;
-    }
-    return i;
+int main(__attribute__((private(0))) int a, __attribute__((private(1))) int b) { 
+   return a & 1;
 }
-
-int main(__attribute__((private(0))) int a, __attribute__((private(1))) int b) {
-	return t();
-} 
