d12a7fb0231bc37c0ad966baaac7e3e11f576c6e
Clive2312 <clivehaha@outlook.com>
2022-07-06 17:58:16 +0000

    db_join test

--- examples/C/mpc/benchmarks/db/db_join.c
+++ examples/C/mpc/benchmarks/db/db_join.c
@@ -2,8 +2,8 @@
  * Example on how to merge two data sets and to perform various analyses
  */
 
-#define LEN_A 20
-#define LEN_B 20
+#define LEN_A 10
+#define LEN_B 10
 
 #define ATT_A 2 //Number of attributes
 #define ATT_B 2
