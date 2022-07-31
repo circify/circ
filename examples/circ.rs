d6a2feef4b70e17faa81b04237dff0dd0395837a
Edward Chen <edwjchen@gmail.com>
2022-07-30 21:45:40 -0400

    Cleaned trans.rs and added benchmark logging

--- examples/circ.rs
+++ examples/circ.rs
@@ -166,7 +166,8 @@ fn determine_language(l: &Language, input_path: &Path) -> DeterminedLanguage {
 }
 
 fn main() {
-    let now = Instant::now();
+    let start = Instant::now();
+    let mut now = Instant::now();
 
     env_logger::Builder::from_default_env()
         .format_level(false)
@@ -175,6 +176,25 @@ fn main() {
     let options = Options::from_args();
     let path_buf = options.path.clone();
     println!("{:?}", options);
+
+    #[cfg(feature = "bench")]
+    {
+        println!(
+            "LOG: Test: {}",
+            options.path.as_path().display().to_string()
+        );
+        match &options.backend {
+            Backend::Mpc {
+                cost_model,
+                selection_scheme,
+            } => {
+                println!("LOG: CostModel: {}", cost_model);
+                println!("LOG: SelectionScheme: {}", selection_scheme);
+            }
+            _ => {}
+        }
+    }
+
     let mode = match options.backend {
         Backend::R1cs { .. } => match options.frontend.value_threshold {
             Some(t) => Mode::ProofOfHighValue(t),
@@ -225,8 +245,10 @@ fn main() {
         }
     };
 
-    println!("Time: Frontend: {:?}", now.elapsed());
+    #[cfg(feature = "bench")]
+    println!("LOG: Frontend: {:?}", now.elapsed());
 
+    now = Instant::now();
     cs = match mode {
         Mode::Opt => opt(
             cs,
@@ -284,6 +306,9 @@ fn main() {
             ],
         ),
     };
+    #[cfg(feature = "bench")]
+    println!("LOG: Optimizations: {:#?}", now.elapsed());
+
     println!("Done with IR optimization");
 
     // for (name, c) in &cs.computations {
@@ -293,6 +318,7 @@ fn main() {
     //     }
     // }
 
+    now = Instant::now();
     match options.backend {
         #[cfg(feature = "r1cs")]
         Backend::R1cs {
@@ -394,5 +420,11 @@ fn main() {
         Backend::Smt { .. } => {
             panic!("Missing feature: smt");
         }
+    }
+
+    #[cfg(feature = "bench")]
+    {
+        println!("LOG: Lowering: {:#?}", now.elapsed());
+        println!("LOG: Compile: {:#?}", start.elapsed());
     }
 }
