480ef732437f03d65935fdb6108a17aa09190ae3
Clive2312 <clivehaha@outlook.com>
2022-08-16 00:34:51 +0000

    update test suite

--- scripts/aby_tests/c_benchmark_aby.py
+++ scripts/aby_tests/c_benchmark_aby.py
@@ -4,8 +4,12 @@ from util import run_tests
 from test_suite import *
 
 if __name__ == "__main__":
-    tests = mnist_tests
-#             + db_tests \
-#             + mnist_tests \
-#             + cryptonets_tests
+    tests = biomatch_tests + \
+            kmeans_tests_2 + \
+            gauss_tests + \
+            db_tests + \
+            mnist_tests + \
+            cryptonets_tests + \
+            histogram_tests + \
+            gcd_tests
     run_tests('c', tests)
