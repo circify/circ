9350d8b930b875a6749a9f6e66b2bdc05dc07510
Edward Chen <edwjchen@gmail.com>
2022-10-12 11:08:20 -0400

    revert unsigned

--- examples/C/mpc/benchmarks/biomatch/2pc_biomatch.c
+++ examples/C/mpc/benchmarks/biomatch/2pc_biomatch.c
@@ -1,32 +1,32 @@
-unsigned main(
-    __attribute__((private(0))) unsigned db[1024],
-    __attribute__((private(1))) unsigned sample[4])
+int main(
+    __attribute__((private(0))) int db[1024],
+    __attribute__((private(1))) int sample[4])
 {
-    unsigned N = 256;
-    unsigned K = 4;
-    unsigned matches[N];
+    int N = 256;
+    int K = 4;
+    int matches[N];
 
     // Compute distances
-    for (unsigned i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
-        unsigned db_inner[K];
-        for (unsigned j = 0; j < K; j++)
+        int db_inner[K];
+        for (int j = 0; j < K; j++)
         {
             db_inner[j] = db[i * K + j];
         }
 
-        unsigned r = 0;
-        for (unsigned k = 0; k < K; k++)
+        int r = 0;
+        for (int k = 0; k < K; k++)
         {
-            unsigned t = (db_inner[k] - sample[k]);
+            int t = (db_inner[k] - sample[k]);
             r += t * t;
         }
         matches[i] = r;
     }
 
     // Compute minimum
-    unsigned best_match = matches[0];
-    for (unsigned l = 1; l < N; l++)
+    int best_match = matches[0];
+    for (int l = 1; l < N; l++)
     {
         if (matches[l] < best_match)
         {
--- examples/C/mpc/benchmarks/kmeans/2pc_kmeans.c
+++ examples/C/mpc/benchmarks/kmeans/2pc_kmeans.c
@@ -1,79 +1,79 @@
-unsigned main(__attribute__((private(0))) unsigned a[20], __attribute__((private(1))) unsigned b[20])
+int main(__attribute__((private(0))) int a[20], __attribute__((private(1))) int b[20])
 {
-    unsigned D = 2;
-    unsigned NA = 10;
-    unsigned NB = 10;
-    unsigned NC = 5;
-    unsigned PRECISION = 4;
-    unsigned LEN = NA + NB;
-    unsigned LEN_OUTER = 10;
-    unsigned LEN_INNER = LEN / LEN_OUTER;
+    int D = 2;
+    int NA = 10;
+    int NB = 10;
+    int NC = 5;
+    int PRECISION = 4;
+    int LEN = NA + NB;
+    int LEN_OUTER = 10;
+    int LEN_INNER = LEN / LEN_OUTER;
 
     // init data
-    unsigned data[LEN * D];
-    for (unsigned i_0 = 0; i_0 < D * NA; i_0++)
+    int data[LEN * D];
+    for (int i_0 = 0; i_0 < D * NA; i_0++)
     {
         data[i_0] = a[i_0];
     }
-    unsigned offset = D * NA;
-    for (unsigned i_1 = 0; i_1 < D * NB; i_1++)
+    int offset = D * NA;
+    for (int i_1 = 0; i_1 < D * NB; i_1++)
     {
         data[i_1 + offset] = b[i_1];
     }
 
-    unsigned output[D * NC];
+    int output[D * NC];
 
     // ======================= kmeans
-    unsigned cluster[D * NC];
+    int cluster[D * NC];
 
     // Assign random start cluster from data
-    for (unsigned i_2 = 0; i_2 < NC; i_2++)
+    for (int i_2 = 0; i_2 < NC; i_2++)
     {
         cluster[i_2 * D] = data[((i_2 + 3) % LEN) * D];
         cluster[i_2 * D + 1] = data[((i_2 + 3) % LEN) * D + 1];
     }
 
-    for (unsigned i_3 = 0; i_3 < PRECISION; i_3++)
+    for (int i_3 = 0; i_3 < PRECISION; i_3++)
     {
-        unsigned new_cluster[D * NC];
+        int new_cluster[D * NC];
 
         // ======================= iteration_unrolled_outer
-        unsigned count[NC];
+        int count[NC];
 
         // Set Outer result
-        for (unsigned i_4 = 0; i_4 < NC; i_4++)
+        for (int i_4 = 0; i_4 < NC; i_4++)
         {
             new_cluster[i_4 * D] = 0;
             new_cluster[i_4 * D + 1] = 0;
             count[i_4] = 0;
         }
 
-        unsigned loop_clusterD1[NC * LEN_OUTER];
-        unsigned loop_clusterD2[NC * LEN_OUTER];
-        unsigned loop_count[NC * LEN_OUTER];
+        int loop_clusterD1[NC * LEN_OUTER];
+        int loop_clusterD2[NC * LEN_OUTER];
+        int loop_count[NC * LEN_OUTER];
 
         // Compute decomposition
-        for (unsigned i_5 = 0; i_5 < LEN_OUTER; i_5++)
+        for (int i_5 = 0; i_5 < LEN_OUTER; i_5++)
         {
             // Copy data, fasthack for scalability
-            unsigned data_offset = i_5 * LEN_INNER * D;
-            unsigned data_inner[LEN_INNER * D];
+            int data_offset = i_5 * LEN_INNER * D;
+            int data_inner[LEN_INNER * D];
 
             // memcpy(data_inner, data+data_offset, LEN_INNER*D*sizeof(coord_t));
-            for (unsigned i_6 = 0; i_6 < LEN_INNER * D; i_6++)
+            for (int i_6 = 0; i_6 < LEN_INNER * D; i_6++)
             {
                 data_inner[i_6] = data[i_6 + data_offset];
             }
 
-            unsigned cluster_inner[NC * D];
-            unsigned count_inner[NC];
+            int cluster_inner[NC * D];
+            int count_inner[NC];
 
             // ======================= iteration_unrolled_inner_depth(data_inner, cluster, cluster_inner, count_inner, LEN_INNER, NC);
-            unsigned dist[NC];
-            unsigned pos[NC];
-            unsigned bestMap_inner[LEN_INNER];
+            int dist[NC];
+            int pos[NC];
+            int bestMap_inner[LEN_INNER];
 
-            for (unsigned i_7 = 0; i_7 < NC; i_7++)
+            for (int i_7 = 0; i_7 < NC; i_7++)
             {
                 cluster_inner[i_7 * D] = 0;
                 cluster_inner[i_7 * D + 1] = 0;
@@ -81,26 +81,26 @@ unsigned main(__attribute__((private(0))) unsigned a[20], __attribute__((private
             }
 
             // Compute nearest clusters for Data item i
-            for (unsigned i_8 = 0; i_8 < LEN_INNER; i_8++)
+            for (int i_8 = 0; i_8 < LEN_INNER; i_8++)
             {
-                unsigned dx = data_inner[i_8 * D];
-                unsigned dy = data_inner[i_8 * D + 1];
+                int dx = data_inner[i_8 * D];
+                int dy = data_inner[i_8 * D + 1];
 
-                for (unsigned i_9 = 0; i_9 < NC; i_9++)
+                for (int i_9 = 0; i_9 < NC; i_9++)
                 {
                     pos[i_9] = i_9;
-                    unsigned x1 = cluster[D * i_9];
-                    unsigned y1 = cluster[D * i_9 + 1];
-                    unsigned x2 = dx;
-                    unsigned y2 = dy;
+                    int x1 = cluster[D * i_9];
+                    int y1 = cluster[D * i_9 + 1];
+                    int x2 = dx;
+                    int y2 = dy;
                     dist[i_9] = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
                 }
                 // hardcoded NC = 5;
                 // stride = 1
                 // stride = 2
                 // stride = 4
-                unsigned stride = 1;
-                for (unsigned i_10 = 0; i_10 < NC - stride; i_10 += 2)
+                int stride = 1;
+                for (int i_10 = 0; i_10 < NC - stride; i_10 += 2)
                 {
                     if (dist[i_10 + stride] < dist[i_10])
                     {
@@ -109,7 +109,7 @@ unsigned main(__attribute__((private(0))) unsigned a[20], __attribute__((private
                     }
                 }
                 stride = 2u;
-                for (unsigned i_11 = 0; i_11 < NC - stride; i_11 += 4)
+                for (int i_11 = 0; i_11 < NC - stride; i_11 += 4)
                 {
                     if (dist[i_11 + stride] < dist[i_11])
                     {
@@ -118,7 +118,7 @@ unsigned main(__attribute__((private(0))) unsigned a[20], __attribute__((private
                     }
                 }
                 stride = 4u;
-                for (unsigned i_12 = 0; i_12 < NC - stride; i_12 += 8)
+                for (int i_12 = 0; i_12 < NC - stride; i_12 += 8)
                 {
                     if (dist[i_12 + stride] < dist[i_12])
                     {
@@ -127,14 +127,14 @@ unsigned main(__attribute__((private(0))) unsigned a[20], __attribute__((private
                     }
                 }
                 bestMap_inner[i_8] = pos[0];
-                unsigned cc = bestMap_inner[i_8];
+                int cc = bestMap_inner[i_8];
                 cluster_inner[cc * D] += data_inner[i_8 * D];
                 cluster_inner[cc * D + 1] += data_inner[i_8 * D + 1];
                 count_inner[cc] += 1;
             }
             // // ======================= iteration_unrolled_inner_depth(data_inner, cluster, cluster_inner, count_inner, LEN_INNER, NC);
 
-            for (unsigned i_13 = 0; i_13 < NC; i_13++)
+            for (int i_13 = 0; i_13 < NC; i_13++)
             {
                 loop_clusterD1[i_13 * LEN_OUTER + i_5] = cluster_inner[i_13 * D];
                 loop_clusterD2[i_13 * LEN_OUTER + i_5] = cluster_inner[i_13 * D + 1];
@@ -142,7 +142,7 @@ unsigned main(__attribute__((private(0))) unsigned a[20], __attribute__((private
             }
         }
 
-        for (unsigned i_14 = 0; i_14 < NC; i_14++)
+        for (int i_14 = 0; i_14 < NC; i_14++)
         {
             new_cluster[i_14 * D] =
                 loop_clusterD1[i_14 * LEN_OUTER + 0] + loop_clusterD1[i_14 * LEN_OUTER + 1] +
@@ -168,7 +168,7 @@ unsigned main(__attribute__((private(0))) unsigned a[20], __attribute__((private
 
         // Recompute cluster Pos
         // Compute mean
-        for (unsigned i_15 = 0; i_15 < NC; i_15++)
+        for (int i_15 = 0; i_15 < NC; i_15++)
         {
             if (count[i_15] > 0)
             {
@@ -179,13 +179,13 @@ unsigned main(__attribute__((private(0))) unsigned a[20], __attribute__((private
         // ======================= iteration_unrolled_outer
 
         // We need to copy inputs to outputs
-        for (unsigned i_16 = 0; i_16 < NC * D; i_16++)
+        for (int i_16 = 0; i_16 < NC * D; i_16++)
         {
             cluster[i_16] = new_cluster[i_16];
         }
     }
 
-    for (unsigned i_17 = 0; i_17 < NC; i_17++)
+    for (int i_17 = 0; i_17 < NC; i_17++)
     {
         output[i_17 * D] = cluster[i_17 * D];
         output[i_17 * D + 1] = cluster[i_17 * D + 1];
@@ -193,8 +193,8 @@ unsigned main(__attribute__((private(0))) unsigned a[20], __attribute__((private
     // ======================= kmeans
     // return output[0];
 
-    unsigned sum = 0;
-    for (unsigned i_18 = 0; i_18 < D * NC; i_18++)
+    int sum = 0;
+    for (int i_18 = 0; i_18 < D * NC; i_18++)
     {
         sum += output[i_18];
     }
