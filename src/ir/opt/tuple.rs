be67fc53462ac82d1c0c3241af921bb1ef22b60c
Alex Ozdemir <aozdemir@hmc.edu>
2023-03-14 00:14:30 -0700

    update some opt passes w/ new ops

--- src/ir/opt/tuple.rs
+++ src/ir/opt/tuple.rs
@@ -106,6 +106,17 @@ impl TupleTree {
     fn bimap(&self, mut f: impl FnMut(Term, Term) -> Term, other: &Self) -> Self {
         self.structure(itertools::zip_eq(self.flatten(), other.flatten()).map(|(a, b)| f(a, b)))
     }
+    fn transpose_map(vs: Vec<Self>, f: impl FnMut(Vec<Term>) -> Term) -> Self {
+        assert!(!vs.is_empty());
+        let n = vs[0].flatten().count();
+        let mut ts = vec![Vec::new(); n];
+        for v in &vs {
+            for (i, t) in v.flatten().enumerate() {
+                ts[i].push(t);
+            }
+        }
+        vs[0].structure(ts.into_iter().map(f))
+    }
     fn get(&self, i: usize) -> Self {
         match self {
             TupleTree::NonTuple(cs) => {
@@ -249,6 +260,15 @@ pub fn eliminate_tuples(cs: &mut Computation) {
                 let a = cs.pop().unwrap();
                 debug_assert!(cs.is_empty());
                 a.bimap(|a, v| term![Op::Store; a, i.clone(), v], &v)
+            }
+            Op::Array(k, _v) => TupleTree::transpose_map(cs, |children| {
+                assert!(!children.is_empty());
+                let v_s = check(&children[0]);
+                term(Op::Array(k.clone(), v_s), children)
+            }),
+            Op::Fill(key_sort, size) => {
+                let values = cs.pop().unwrap();
+                values.map(|v| term![Op::Fill(key_sort.clone(), *size); v])
             }
             Op::Select => {
                 let i = cs.pop().unwrap().unwrap_non_tuple();
--- src/ir/opt/visit.rs
+++ src/ir/opt/visit.rs
@@ -1,5 +1,7 @@
 use crate::ir::term::*;
 
+use log::trace;
+
 /// A rewriting pass.
 pub trait RewritePass {
     /// Visit (and possibly rewrite) a term.
@@ -11,11 +13,49 @@ pub trait RewritePass {
         orig: &Term,
         rewritten_children: F,
     ) -> Option<Term>;
+
+    /// What
+    fn visit_cache(
+        &mut self,
+        computation: &mut Computation,
+        orig: &Term,
+        cache: &TermMap<Term>,
+    ) -> Option<Term> {
+        let get_children = || -> Vec<Term> {
+            orig.cs()
+                .iter()
+                .map(|c| cache.get(c).unwrap())
+                .cloned()
+                .collect()
+        };
+        self.visit(computation, orig, get_children)
+    }
+
     fn traverse(&mut self, computation: &mut Computation) {
+        self.traverse_full(computation, false, true);
+    }
+
+    fn traverse_full(
+        &mut self,
+        computation: &mut Computation,
+        precompute: bool,
+        persistent_arrays: bool,
+    ) {
         let mut cache = TermMap::<Term>::default();
         let mut children_added = TermSet::default();
         let mut stack = Vec::new();
+        if persistent_arrays {
+            stack.extend(
+                computation
+                    .persistent_arrays
+                    .iter()
+                    .map(|(_name, final_term)| final_term.clone()),
+            );
+        }
         stack.extend(computation.outputs.iter().cloned());
+        if precompute {
+            stack.extend(computation.precomputes.outputs().values().cloned());
+        }
         while let Some(top) = stack.pop() {
             if !cache.contains_key(&top) {
                 // was it missing?
@@ -23,17 +63,26 @@ pub trait RewritePass {
                     stack.push(top.clone());
                     stack.extend(top.cs().iter().filter(|c| !cache.contains_key(c)).cloned());
                 } else {
-                    let get_children = || -> Vec<Term> {
-                        top.cs()
-                            .iter()
-                            .map(|c| cache.get(c).unwrap())
-                            .cloned()
-                            .collect()
-                    };
-                    let new_t_opt = self.visit(computation, &top, get_children);
-                    let new_t = new_t_opt.unwrap_or_else(|| term(top.op().clone(), get_children()));
+                    let new_t_opt = self.visit_cache(computation, &top, &cache);
+                    let new_t = new_t_opt.unwrap_or_else(|| {
+                        term(
+                            top.op().clone(),
+                            top.cs()
+                                .iter()
+                                .map(|c| cache.get(c).unwrap())
+                                .cloned()
+                                .collect(),
+                        )
+                    });
                     cache.insert(top.clone(), new_t);
                 }
+            }
+        }
+        if persistent_arrays {
+            for (_name, final_term) in &mut computation.persistent_arrays {
+                let new_final_term = cache.get(final_term).unwrap().clone();
+                trace!("Array {} -> {}", final_term, new_final_term);
+                *final_term = new_final_term;
             }
         }
         computation.outputs = computation
@@ -41,6 +90,13 @@ pub trait RewritePass {
             .iter()
             .map(|o| cache.get(o).unwrap().clone())
             .collect();
+        if precompute {
+            let os = computation.precomputes.outputs().clone();
+            for (name, old_term) in os {
+                let new_term = cache.get(&old_term).unwrap().clone();
+                computation.precomputes.change_output(&name, new_term);
+            }
+        }
     }
 }
 
